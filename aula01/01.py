a531bb0eee3a75f68d0e768e9f8f1310645489ea
Adriel <adriel.fsantos@outlook.com>
2022-05-05 21:16:48 -0300

    Adiciona resolucao do execicio 1 da aula 1

---
+++ aula01/01.py
@@ -0,0 +1,10 @@
+nome = input("Digite seu nome: ")
+cpf = input("Digite seu cpf: ")
+uf = input("Digite sua unidade federativa: ")
+
+print("-" * 40)
+print("Confirmação de cadastro:")
+print("Nome:", nome)
+print("CPF:", cpf)
+print("UF:", uf)
+print("-" * 40)
